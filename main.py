78518f5df4fdac18a4b8453b39c2c17689511296
bvdepeutte <bvdepeut@gmail.com>
2024-03-09 13:14:06 +0100

    main, get_book_text, get°list def

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+def main():
+    book_path = "books/Frankenstein.txt"
+    text = get_book_text(book_path)
+    # print(text)
+    words = len(get_list(text))
+    print(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_list(book_text):
+    words = []
+    words = book_text.split()
+    # print(words)
+    return words
+
+
+
+main()
